25318d1690c9b89b1baee0176c8ffcaa259a1dea
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-27 21:59:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,9 +32,9 @@ class Song
     song = self.new(name)
     song
   end
-  def self.find_by_name(name)
-    self.all.find {|song| song.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.find {|song| song.name == name}
+  # end
   def self.find_or_create_by_name(name)
    self.find_by_name(name) || self.create(name)
 
